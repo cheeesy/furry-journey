59fddd603ce16c4457996ad3676396583e214d85
cheeesy <cheeesy@aaathats3as.com>
2019-10-11 19:03:45 +0200

    Fixed FormulateResponse() in TherapistResponseMaker.py

--- Therapist/TherapistResponseMaker.py
+++ Therapist/TherapistResponseMaker.py
@@ -8,7 +8,7 @@ debug = False        # Will print the Response and Keywords etc.
 
 def CheckForArray(string):
     for i in range(0,len(keywords)):
-        if(string.lower()==keywords[i]):
+        if(keywords[i] in string.lower()):
             if(debug==True):
                 print("Keyword detected!")
             return string
@@ -17,9 +17,8 @@ def SendOut(response):
     print(response)
         
 def FormulateResponse(key):
-    if key == "Hello":
+    if keywords[0] in key.lower():
         return "We already greeted...\n"
-    
 
 def Thinker(resp):
     if(isinstance(resp, str) == True):
